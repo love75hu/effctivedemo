4b033884cf2d40b557435c060b6466a40d95c844
caoyueyue <caoyueyue@mediinfo.com.cn>
2023-06-06 12:02:06 +0800

    DCDT31509 新增收藏夹，新增收藏夹明细，更新收藏夹，移除某一条收藏夹明细，作废收藏夹等接口

--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/controller/ShuJuZXSCController.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/controller/ShuJuZXSCController.java
@@ -1,11 +1,20 @@
 package cn.mediinfo.grus.shujuzx.controller;
 
+import cn.mediinfo.grus.shujuzx.dto.ShuJuZXSCs.SC_SC_ShouCangJMXInDto;
+import cn.mediinfo.grus.shujuzx.dto.ShuJuZXSCs.SC_SC_ShouCangJMXOutDto;
+import cn.mediinfo.grus.shujuzx.dto.ShuJuZXSCs.SC_SC_ShouCangJXXInDto;
+import cn.mediinfo.grus.shujuzx.dto.ShuJuZXSCs.SC_SC_ShouCangJXXOutDto;
 import cn.mediinfo.grus.shujuzx.service.ShuJuZXWDSCService;
+import cn.mediinfo.starter.base.exception.TongYongYWException;
+import cn.mediinfo.starter.base.response.MsfResponse;
+import cn.mediinfo.starter.base.util.StringUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Tag(name = "ShuJuZXSCController", description = "数据中心-收藏夹")
@@ -19,4 +28,54 @@ public class ShuJuZXSCController {
         this.shuJuZXWDSCService=shuJuZXWDSCService;
     }
 
+    @Operation(summary = "新增收藏夹")
+    @PostMapping("AddShouCangJia")
+    public MsfResponse<Integer> addShouCangJia(@RequestBody SC_SC_ShouCangJXXInDto shouCangJInDto) throws TongYongYWException {
+        if (!StringUtil.hasText(shouCangJInDto.getShouCangJMC())) {
+            return MsfResponse.fail("收藏夹名称不能为空！");
+        }
+        return MsfResponse.success(shuJuZXWDSCService.addShouCangJia(shouCangJInDto));
+    }
+
+    @Operation(summary = "新增收藏夹明细")
+    @PostMapping("AddShouCangJMX")
+    public MsfResponse<Integer> addShouCangJMX(@RequestBody SC_SC_ShouCangJMXInDto shouCangJMXInDto) {
+        return MsfResponse.success(shuJuZXWDSCService.addShouCangJMX(shouCangJMXInDto));
+    }
+
+    @Operation(summary = "更新收藏夹")
+    @PostMapping("UpdateShouCangJia")
+    public MsfResponse<Integer> updateShouCangJia(@RequestBody SC_SC_ShouCangJXXInDto shouCangJInDto) throws TongYongYWException {
+        return MsfResponse.success(shuJuZXWDSCService.updateShouCangJia(shouCangJInDto));
+    }
+
+    @Operation(summary = "移除某一条收藏夹明细")
+    @DeleteMapping("YiChuShouCangJMX")
+    public MsfResponse<Integer> yiChuShouCangJMX(String id) throws TongYongYWException {
+        return MsfResponse.success(shuJuZXWDSCService.yiChuShouCangJMX(id));
+    }
+
+    @Operation(summary = "作废收藏夹")
+    @DeleteMapping("ZuoFeiShouCangJia")
+    public MsfResponse<Integer> zuoFeiShouCangJia(String id) throws TongYongYWException {
+        return MsfResponse.success(shuJuZXWDSCService.zuoFeiShouCangJia(id));
+    }
+
+    @Operation(summary = "获取当前登录人的收藏夹列表")
+    @GetMapping("GetShouCangJiaList")
+    public MsfResponse<List<SC_SC_ShouCangJXXOutDto>> getShouCangJiaList(String likeQuery) {
+        return MsfResponse.success(shuJuZXWDSCService.getShouCangJiaList(likeQuery));
+    }
+
+    @Operation(summary = "根据收藏夹id和其他查询条件获取收藏夹明细条数")
+    @GetMapping("GetShouCangJMXCount")
+    public MsfResponse<Integer> getShouCangJMXCount(String likeQuery, String shouCangJID) {
+        return MsfResponse.success(shuJuZXWDSCService.getShouCangJMXCount(likeQuery, shouCangJID));
+    }
+
+    @Operation(summary = "根据收藏夹id和其他查询条件获取收藏夹明细")
+    @GetMapping("GetShouCangJMXList")
+    public MsfResponse<List<SC_SC_ShouCangJMXOutDto>> getShouCangJMXList(String likeQuery, String shouCangJID, Integer pageIndex, Integer pageSize) {
+        return MsfResponse.success(shuJuZXWDSCService.getShouCangJMXList(likeQuery, shouCangJID, pageIndex, pageSize));
+    }
 }
--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/service/ShuJuZXWDSCService.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/service/ShuJuZXWDSCService.java
@@ -1,4 +1,80 @@
 package cn.mediinfo.grus.shujuzx.service;
 
+import cn.mediinfo.grus.shujuzx.dto.ShuJuZXSCs.SC_SC_ShouCangJMXInDto;
+import cn.mediinfo.grus.shujuzx.dto.ShuJuZXSCs.SC_SC_ShouCangJMXOutDto;
+import cn.mediinfo.grus.shujuzx.dto.ShuJuZXSCs.SC_SC_ShouCangJXXInDto;
+import cn.mediinfo.grus.shujuzx.dto.ShuJuZXSCs.SC_SC_ShouCangJXXOutDto;
+import cn.mediinfo.starter.base.exception.TongYongYWException;
+
+import java.util.List;
+
 public interface ShuJuZXWDSCService {
+    /**
+     * 新增收藏夹
+     *
+     * @param shouCangJInDto
+     * @return
+     */
+    Integer addShouCangJia(SC_SC_ShouCangJXXInDto shouCangJInDto) throws TongYongYWException;
+
+    /**
+     * 新增收藏夹明细
+     *
+     * @param shouCangJMXInDto
+     * @return
+     */
+    Integer addShouCangJMX(SC_SC_ShouCangJMXInDto shouCangJMXInDto);
+
+    /**
+     * 更新收藏夹
+     *
+     * @param shouCangJInDto
+     * @return
+     */
+    Integer updateShouCangJia(SC_SC_ShouCangJXXInDto shouCangJInDto) throws TongYongYWException;
+
+    /**
+     * 移除某一条收藏夹明细
+     *
+     * @param id
+     * @return
+     */
+    Integer yiChuShouCangJMX(String id) throws TongYongYWException;
+
+    /**
+     * 作废收藏夹
+     *
+     * @param id
+     * @return
+     */
+    Integer zuoFeiShouCangJia(String id) throws TongYongYWException;
+
+    /**
+     * 获取当前登录人的收藏夹列表
+     *
+     * @param likeQuery
+     * @return
+     */
+    List<SC_SC_ShouCangJXXOutDto> getShouCangJiaList(String likeQuery);
+
+    /**
+     * 根据收藏夹id和其他查询条件获取收藏夹明细条数
+     *
+     * @param likeQuery
+     * @param shouCangJID
+     * @return
+     */
+    Integer getShouCangJMXCount(String likeQuery, String shouCangJID);
+
+    /**
+     * 根据收藏夹id和其他查询条件获取收藏夹明细
+     *
+     * @param likeQuery
+     * @param shouCangJID
+     * @param pageIndex
+     * @param pageSize
+     * @return
+     */
+    List<SC_SC_ShouCangJMXOutDto> getShouCangJMXList(String likeQuery, String shouCangJID, Integer pageIndex, Integer pageSize);
+
 }

55f3abdc12de700b1bdb731242988d9161be98f7
llc <838636046@qq.com>
2023-10-09 15:45:21 +0800

    DBDT31239 调整控制器

--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/controller/FangAnXXController.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/controller/FangAnXXController.java
@@ -1,11 +1,14 @@
 package cn.mediinfo.grus.shujuzx.controller;
 
 import cn.mediinfo.cyan.msf.core.response.MsfResponse;
+import cn.mediinfo.grus.shujuzx.dto.chaxunfaxx.FangAnCXLSDto;
+import cn.mediinfo.grus.shujuzx.dto.chaxunfaxx.FangAnXXDto;
 import cn.mediinfo.grus.shujuzx.dto.fangan.FangAnQueryDTO;
 import cn.mediinfo.grus.shujuzx.request.FangAnCXLSSaveRequest;
 import cn.mediinfo.grus.shujuzx.request.fangan.FangAnQuerySqlRequest;
 import cn.mediinfo.grus.shujuzx.request.fangan.FangAnXXSaveRequest;
 import cn.mediinfo.grus.shujuzx.request.fangan.FangAnXXUpdateRequest;
+import cn.mediinfo.grus.shujuzx.service.ChaXunFAXXService;
 import cn.mediinfo.grus.shujuzx.service.FangAnService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @Tag(name = "FangAnXXController", description = "方案信息")
 @RequestMapping({"api/v1.0/fanganxx", "api/v1/fanganxx"})
@@ -23,6 +28,8 @@ public class FangAnXXController {
 
     @Autowired
     private FangAnService fangAnService;
+    @Autowired
+    private ChaXunFAXXService chaXunFAXXService;
 
     @Operation(summary = "保存方案")
     @PostMapping("/save")
@@ -53,5 +60,58 @@ public class FangAnXXController {
     @PostMapping("/saveQueryRercord")
     public MsfResponse<String> saveFangAnCXLSSaveRequest(@Validated @RequestBody FangAnCXLSSaveRequest request){
         return MsfResponse.success();
+    }
+
+    /**
+     * 搜索查询方案下拉选择
+     *
+     */
+    @Operation(summary = "搜索查询方案下拉选择")
+    @GetMapping("GetChaXunFAXXSelect")
+    public MsfResponse<List<FangAnXXDto>> getChaXunFAXXSelect(@RequestParam(required = false) String likeQuery,
+                                                              @RequestParam(required = false) String fangAnLXDM,
+                                                              @RequestParam(required = false,defaultValue = "1") Integer pageIndex,
+                                                              @RequestParam(required = false,defaultValue = "10") Integer pageSize) {
+        return MsfResponse.success(chaXunFAXXService.getChaXunFAXXSelect(likeQuery, fangAnLXDM, pageIndex, pageSize));
+    }
+    /**
+     * 获取方案信息记录
+     *
+     */
+    @Operation(summary = "获取方案信息记录")
+    @GetMapping("GetFangAnXXList")
+    public MsfResponse<List<FangAnXXDto>> getFangAnXXList(@RequestParam(required = false) String likeQuery,
+                                                          @RequestParam(required = false,defaultValue = "1") Integer pageIndex,
+                                                          @RequestParam(required = false,defaultValue = "10") Integer pageSize) {
+        return MsfResponse.success(chaXunFAXXService.getFangAnXXList(likeQuery, pageIndex, pageSize));
+    }
+    /**
+     * 获取方案查询历史记录
+     *
+     */
+    @Operation(summary = "获取方案查询历史记录")
+    @GetMapping("GetFangAnCXLSList")
+    public MsfResponse<List<FangAnCXLSDto>> getFangAnCXLSList(@RequestParam(required = false,defaultValue = "1") Integer pageIndex,
+                                                              @RequestParam(required = false,defaultValue = "10") Integer pageSize) {
+        return MsfResponse.success(chaXunFAXXService.getFangAnCXLSList(pageIndex, pageSize));
+    }
+    /**
+     * 作废查询方案历史
+     *
+     */
+    @Operation(summary = "作废查询方案历史")
+    @DeleteMapping("DeleteFangAnCXLS")
+    public MsfResponse<Boolean> deleteFangAnCXLS(@RequestParam String id) {
+        return MsfResponse.success(chaXunFAXXService.deleteFangAnCXLS(id));
+    }
+
+    /**
+     * 作废查询方案
+     *
+     */
+    @Operation(summary = "作废查询方案")
+    @DeleteMapping("DeleteChaXunFA")
+    public MsfResponse<Boolean> deleteChaXunFA(@RequestParam String id) {
+        return MsfResponse.success(chaXunFAXXService.deleteChaXunFA(id));
     }
 }

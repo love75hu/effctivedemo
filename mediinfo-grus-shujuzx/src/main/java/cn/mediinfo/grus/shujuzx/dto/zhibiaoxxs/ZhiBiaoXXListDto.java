9d01c8eb39834c980facacc3ec23bc32319e9cc1
llc <838636046@qq.com>
2023-10-11 10:02:38 +0800

    DBDT31239 调整获取指标药品信息接口及校验调整

--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/dto/zhibiaoxxs/ZhiBiaoXXListDto.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/dto/zhibiaoxxs/ZhiBiaoXXListDto.java
@@ -2,16 +2,19 @@ package cn.mediinfo.grus.shujuzx.dto.zhibiaoxxs;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 
 @Data
 public class ZhiBiaoXXListDto {
+    @Schema(description = "主键")
+    private String id;
     @Schema(description = "指标分类ID")
     private String zhiBiaoFLID;
     @Schema(description = "指标分类名称")
     private String zhiBiaoFLMC;
+    @Schema(description = "顺序号")
+    private Integer shunXuHao;
     @Schema(description = "指标明细集合")
     private List<ZhiBiaoMXListDto> zhiBiaoMXList;
 }

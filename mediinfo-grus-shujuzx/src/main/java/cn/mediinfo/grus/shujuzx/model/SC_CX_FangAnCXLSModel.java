bd1b7c5ec0c2cfac6d85492eaa538d7f2a65d476
李国强 <liguoqiang@mediinfo.com.cn>
2023-11-09 05:01:12 +0000

    DBDT31448 model 更新 缺少的字段，更新model 没有注释问题

--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/model/SC_CX_FangAnCXLSModel.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/model/SC_CX_FangAnCXLSModel.java
@@ -1,41 +1,91 @@
 package cn.mediinfo.grus.shujuzx.model;
 
+import lombok.Data;
 import cn.mediinfo.cyan.msf.tenant.orm.entity.StringMTEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 import java.util.Date;
 
+/*** 方案查询历史*/
 @Data
 @Entity
-@Table(name="sc_cx_fangancxls")
+@Table(name = "sc_cx_fangancxls")
 public class SC_CX_FangAnCXLSModel extends StringMTEntity {
-
-   private String zuZhiJGID;
-
-   private String zuZhiJGMC;
-
-   private String chaXunLXDM;
-
-   private String chaXunLXMC;
-
-   private String fangAnID;
-
-   private String fangAnMC;
-
-   private String guanJianZi;
-
-   private String chaXunTJMS;
-
-   private String chaXunSQL;
-
-   private Integer chaXunHS;
-
-   private Date chaXunSJ;
-
-   private String chaXunRID;
-
-   private String chaXunRXM;
-
+    /**
+     * 组织机构ID
+     */
+    @Column(columnDefinition = "组织机构ID")
+    private String zuZhiJGID;
+    /**
+     * 组织机构名称
+     */
+    @Column(columnDefinition = "组织机构名称")
+    private String zuZhiJGMC;
+    /**
+     * 查询类型代码
+     */
+    @Column(columnDefinition = "查询类型代码")
+    private String chaXunLXDM;
+    /**
+     * 查询类型名称(1-方案查询，2-高级查询)
+     */
+    @Column(columnDefinition = "查询类型名称(1-方案查询，2-高级查询)")
+    private String chaXunLXMC;
+    /**
+     * 方案ID
+     */
+    @Column(columnDefinition = "方案ID")
+    private String fangAnID;
+    /**
+     * 方案名称
+     */
+    @Column(columnDefinition = "方案名称")
+    private String fangAnMC;
+    /**
+     * 关键字
+     */
+    @Column(columnDefinition = "关键字")
+    private String guanJianZi;
+    /**
+     * 查询条件描述
+     */
+    @Column(columnDefinition = "查询条件描述")
+    private String chaXunTJMS;
+    /**
+     * 查询条件;JSON字符串
+     */
+    @Column(columnDefinition = "查询条件;JSON字符串")
+    private String chaXunTJ;
+    /**
+     * 查询SQL
+     */
+    @Column(columnDefinition = "查询SQL")
+    private String chaXunSQL;
+    /**
+     * 查询输出：JSON字符串
+     */
+    @Column(columnDefinition = "查询输出：JSON字符串")
+    private String chaXunSC;
+    /**
+     * 查询耗时;单位：ms
+     */
+    @Column(columnDefinition = "查询耗时;单位：ms")
+    private Integer chaXunHS;
+    /**
+     * 查询时间
+     */
+    @Column(columnDefinition = "查询时间")
+    private Date chaXunSJ;
+    /**
+     * 查询人ID
+     */
+    @Column(columnDefinition = "查询人ID")
+    private String chaXunRID;
+    /**
+     * 查询人姓名
+     */
+    @Column(columnDefinition = "查询人姓名")
+    private String chaXunRXM;
 }
--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/model/SC_CX_FangAnNRModel.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/model/SC_CX_FangAnNRModel.java
@@ -1,25 +1,46 @@
 package cn.mediinfo.grus.shujuzx.model;
 
 import cn.mediinfo.cyan.msf.tenant.orm.entity.StringMTEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+/**
+ * 检索方案内容
+ */
 @Data
 @Entity
 @Table(name="sc_cx_fangannr")
 public class SC_CX_FangAnNRModel extends StringMTEntity {
-
+   /**
+    * 组织机构ID
+    */
+   @Column(columnDefinition = "组织机构ID")
    private String zuZhiJGID;
-
+   /**
+    * 组织机构名称
+    */
+   @Column(columnDefinition = "组织机构名称")
    private String zuZhiJGMC;
-
+   /**
+    * 方案ID
+    */
+   @Column(columnDefinition = "方案ID")
    private String fangAnID;
-
+   /**
+    * 方案名称
+    */
+   @Column(columnDefinition = "方案名称")
    private String fangAnMC;
-
+   /**
+    * 查询条件;JSON字符串
+    */
+   @Column(columnDefinition = "查询条件;JSON字符串")
    private String chaXunTJ;
-
+   /**
+    * 查询SQL
+    */
+   @Column(columnDefinition = "查询SQL")
    private String chaXunSQL;
-
 }

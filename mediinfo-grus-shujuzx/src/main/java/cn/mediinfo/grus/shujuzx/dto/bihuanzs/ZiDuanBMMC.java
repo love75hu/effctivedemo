3bde550a2f7ecec280be2916761fcacca12c471c
李国强 <liguoqiang@mediinfo.com.cn>
2023-11-21 08:04:33 +0000

    DBPR12300 闭环展示 接口定义 dto 定义

---
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/dto/bihuanzs/ZiDuanBMMC.java
@@ -0,0 +1,15 @@
+package cn.mediinfo.grus.shujuzx.dto.bihuanzs;
+
+import lombok.Data;
+
+@Data
+public class ZiDuanBMMC {
+    /**
+     * 字段编码
+     */
+    private String ziDuanBM;
+    /**
+     * 字段名称
+     */
+    private String ziDuanZhi;
+}
---
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/dto/bihuanzs/jieDianNRList.java
@@ -0,0 +1,27 @@
+package cn.mediinfo.grus.shujuzx.dto.bihuanzs;
+
+import lombok.Data;
+
+@Data
+public class jieDianNRList {
+    /**
+     * 字段编码
+     */
+    private String ziDuanBM;
+    /**
+     * 字段名称
+     */
+    private String ziDuanMC;
+    /**
+     * 字段值
+     */
+    private String ziDuanZhi;
+    /**
+     * 控制时间标志
+     */
+    private String kongZhiSJBZ;
+    /**
+     * 允许为空标志
+     */
+    private String yunXuWKBZ;
+}

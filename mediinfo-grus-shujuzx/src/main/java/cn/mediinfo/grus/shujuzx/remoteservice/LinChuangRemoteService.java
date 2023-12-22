ca5e200140ec9051de52999a93df84416632efc1
李源均 <liyuanjun@mediinfo.com.cn>
2023-12-22 09:18:26 +0000

    DBDT31855 1、增加调用临床检索服务查询sql的业务逻辑； 2、修改日期转换出错的问题

--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/remoteservice/LinChuangRemoteService.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/remoteservice/LinChuangRemoteService.java
@@ -2,6 +2,7 @@ package cn.mediinfo.grus.shujuzx.remoteservice;
 
 import cn.mediinfo.cyan.msf.core.response.MsfResponse;
 import cn.mediinfo.grus.shujuzx.dto.bihuanlcs.BingLiXQDto;
+import cn.mediinfo.grus.shujuzx.remotedto.linchuang.ChaXunDto;
 import cn.mediinfo.grus.shujuzx.remotedto.linchuang.JiuluTextRso;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Map;
 
 @FeignClient( value = "mediinfo-grus-linchuang-query",path = "mediinfo-grus-linchuang-query")
 public interface LinChuangRemoteService {
@@ -21,5 +23,22 @@ public interface LinChuangRemoteService {
     @PostMapping("/api/v1.0/BingLi/getBiLiJLTextXX")
     MsfResponse<List<JiuluTextRso>> getZuYuanMZJZXX(@RequestBody BingLiXQDto bingLiXQDto);
 
+    /**
+     * 获取数量
+     */
+    @PostMapping("/api/v1.0/ChaXun/getShuLiang")
+    MsfResponse<Long> getShuLiang(@RequestBody ChaXunDto chaXunDto);
+
+    /**
+     * 获取字符串列表
+     */
+    @PostMapping("/api/v1.0/ChaXun/getStringList")
+    MsfResponse<List<String>> getStringList(@RequestBody ChaXunDto chaXunDto);
+
+    /**
+     * 获取字典列表
+     */
+    @PostMapping("/api/v1.0/ChaXun/getZiDianList")
+    MsfResponse<List<Map<String, Object>>> getZiDianList(@RequestBody ChaXunDto chaXunDto);
 
 }

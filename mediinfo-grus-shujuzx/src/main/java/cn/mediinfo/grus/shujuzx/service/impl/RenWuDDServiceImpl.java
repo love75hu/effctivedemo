71d12f2c2633e55963e96cb89dd703f2fae8633d
王艳 <wangyan1@mediinfo.com.cn>
2024-01-06 10:17:47 +0000

    Wangyan dev

--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/service/impl/RenWuDDServiceImpl.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/service/impl/RenWuDDServiceImpl.java
@@ -21,42 +21,7 @@ public class RenWuDDServiceImpl implements RenWuDDService {
         this.renWuGLService=renWuGLService;
     }
     @Override
-    public Boolean saveRenWuZX(String renWuMC){
-        //region
-//        var result = jiBenXXRepository.asQuerydsl()
-//                .where(t->t.renWuMC.eq(renWuMC).and(t.qiYongBZ.eq(1)))
-//                .leftJoin(tongYongPZRepository.asQuerydsl(), (jiBenXX, tongYongPZ) -> jiBenXX.fenLeiDM.eq(tongYongPZ.fenLeiDM), JiBenXXAndTongYongPZPO::new)
-//                .select(q -> new Expression[]{
-//                                q.jiBenXX().id,
-//                                q.jiBenXX().renWuID,
-//                                q.jiBenXX().renWuMC,
-//                                q.jiBenXX().renWuSM,
-//                                q.jiBenXX().fenLeiMC,
-//                                q.jiBenXX().zhiXingPLMC,
-//                                q.jiBenXX().zhiXingZTMC,
-//                                q.jiBenXX().zhiXingZTDM,
-//                                q.jiBenXX().beiZhu,
-//                                q.jiBenXX().zuiJinZXRZID,
-//                                q.jiBenXX().zhiXingHS,
-//                                q.jiBenXX().qiYongBZ,
-//                                q.jiBenXX().zhiXingKSSJ.as("zuiJinZXSJ"),
-//                                q.jiBenXX().renWuDZ,
-//                                q.tongYongPZ().fuWuQIP,
-//                                q.tongYongPZ().fuWuQDK,
-//                                q.tongYongPZ().renWuDZ.as("fuWuQRWDZ")
-//                        }
-//                        , SC_RW_JiBenXXListDto.class).fetchFirst();
-//        if (result!=null){
-//            if (!Objects.isNull(result.getFuWuQIP()) && !Objects.equals(result.getFuWuQIP(),"")
-//            && !Objects.isNull(result.getFuWuQDK()) && !Objects.equals(result.getFuWuQDK(),"")
-//                    && !Objects.isNull(result.getFuWuQRWDZ()) && !Objects.equals(result.getFuWuQRWDZ(),"")) {
-//                if (Objects.isNull(result.getRenWuDZ()) || Objects.equals(result.getRenWuDZ(),"")){
-//
-//                }
-//            }
-//
-//        }
-        //endregion
+    public String saveRenWuZX(String renWuMC){
         var jibenxx=jiBenXXRepository.asQuerydsl().where(t->t.renWuMC.eq(renWuMC).and(t.qiYongBZ.eq(1))).fetchFirst();
 
         if (jibenxx!=null){
@@ -69,7 +34,7 @@ public class RenWuDDServiceImpl implements RenWuDDService {
             renWuGLService.saveRenWuZXList(createDtoList);
         }
 
-        return true;
+        return "执行完成";
     }
 
 }

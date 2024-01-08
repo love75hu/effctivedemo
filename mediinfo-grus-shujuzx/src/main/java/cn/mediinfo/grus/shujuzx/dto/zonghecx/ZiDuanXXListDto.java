ddf0861665a6782f0aed341c6863cf5390e186f9
李国强 <liguoqiang@mediinfo.com.cn>
2024-01-08 02:55:32 +0000

    DBTT31952 修改临床检索配置 添加字段报错问题

--- mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/dto/zonghecx/ZiDuanXXListDto.java
+++ mediinfo-grus-shujuzx/src/main/java/cn/mediinfo/grus/shujuzx/dto/zonghecx/ZiDuanXXListDto.java
@@ -10,9 +10,9 @@ public class ZiDuanXXListDto {
     @Schema(description = "字段名称")
     private String ziDuanMC;
     @Schema(description = "条件标志")
-    private String tiaoJianBZ;
+    private Integer tiaoJianBZ;
     @Schema(description = "输出标志")
-    private String shuChuBZ;
+    private Integer shuChuBZ;
     @Schema(description = "顺序号")
     private String shunXuHao;
 }
